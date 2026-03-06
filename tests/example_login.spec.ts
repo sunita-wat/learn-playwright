90492d8c93da4bb72f2782bded7ccff1cf119713
sunita-wat <sunita42.sn@gmail.com>
2026-03-06 19:52:57 +0700

    ปรับให้โค้ดสั้นลง เพิ่ม test.step
    ปรับให้ Test Report อ่านง่าย

--- tests/example_login.spec.ts
+++ tests/example_login.spec.ts
@@ -1,30 +1,29 @@
 import { test, expect } from '@playwright/test';
 
+    // เพิ่ม test.describe ในกรณีที่รันโค้ดบางอย่างก่อนทุก Test Case 
     test.describe('TS-1 Login page', () => {
-        test('TC-01 Login standard_user secces', async ({ page }) => {
+        test.beforeEach(async ({ page }) => {
             await page.goto('https://www.saucedemo.com/');
-
-            await page.locator('#user-name').fill('standard_user');
-            await expect (page.locator('#user-name')).toHaveValue('standard_user');
-
-            await page.locator('#password').fill('secret_sauce');
-            await expect (page.locator('#password')).toHaveValue('secret_sauce');
-
-            await page.locator('#login-button').click();
-            await expect (page).toHaveURL('https://www.saucedemo.com/inventory.html');
-            await expect (page).toHaveTitle('Swag Labs');
         });
 
-        test('TC-02 locked_out_user', async ({ page }) => {
-            await page.goto('https://www.saucedemo.com/');
-    
-            await page.locator('#user-name').fill('locked_out_user');
-            await expect (page.locator('#user-name')).toHaveValue('locked_out_user');
+        test('TC-01 Login standard_user secces', async ({ page }) => {
 
-            await page.locator('#password').fill('secret_sauce');
-            await expect (page.locator('#password')).toHaveValue('secret_sauce');
+            // เพิ่มการใส่ Step เพื่อให้ตอนรันตรวจสอบอ่านได้ง่ายขึ้น
+            await test.step('Step 1: input username', async () => {
+                await page.locator('#user-name').fill('standard_user');
+            });
 
-            await page.locator('#login-button').click();
-            await expect (page.locator('[data-test="error"]')).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+            await test.step('Step 2: input password', async () => {
+                await page.locator('#password').fill('secret_sauce');
+            });
+
+            await test.step('Step 3: Click login button', async () => {
+                await page.locator('#login-button').click();
+            });
+
+            await test.step('Step 4: Verify login success', async () => {
+                await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+            });
+
         });
     });
